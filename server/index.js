6ed9406aa0eddb0bbbc019e22f562eb1a4939b55
David fox <davidfox967@yahoo.com>
2022-10-14 22:00:19 -0600

    10/14/22

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`server running on poirt ${PORT}`));
+const URI = "mongodb+srv://davidfox:jobsitetracker123@cluster0.pclssj1.mongodb.net/?retryWrites=true&w=majority";
+
+const connection = async () => {
+	try {
+		await mongoose.connect(URI, { useNewUrlParser: true });
+		console.log("connected to database");
+	} catch (error) {
+		console.log(error);
+	}
+};
+connection();
+
+// app.get("/", function (req, res) {
+// 	res.send("Hello World");
+// });
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
