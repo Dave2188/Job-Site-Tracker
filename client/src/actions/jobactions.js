375aecefdff767f4ba996a69b6b53a485ff6ba64
David fox <davidfox967@yahoo.com>
2022-10-17 23:23:02 -0600

    set up more redux

--- client/src/actions/jobactions.js
+++ client/src/actions/jobactions.js
@@ -1,1 +1,12 @@
 import * as api from "../api";
+
+export const getJobs = () => async (dispatch) => {
+	try {
+		const { data } = await api.fetchJobs();
+		console.log(data);
+
+		dispatch({ type: "", payload: data });
+	} catch (error) {
+		console.log(error.mesage);
+	}
+};
