a9ef72ea4482c5e1ac5060806afc952faedf7de5
David fox <davidfox967@yahoo.com>
2023-01-07 21:06:40 -0700

    JST-8 login and out working

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -8,3 +8,11 @@ export const createUser = async (dispatch) => {
 		console.log(error.message);
 	}
 };
+
+export const loginUser = async (dispatch) => {
+	try {
+		const { data } = await api.login();
+	} catch (error) {
+		console.log(error.message);
+	}
+};
